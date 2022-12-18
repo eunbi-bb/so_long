1fb02077cd4871a30100a8168e434826d8d78e73
eunbi-bb <eunbicho1995@gmail.com>
2022-12-18 21:15:34 +0100

    esc key function added

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
-
+#include <unistd.h>
+#include <stdlib.h>
 #include "mlx_linux/mlx.h"
+#include <stdio.h>
 
+//이미지의 정보를 나타내는 변수를 저장한 구조체
 typedef struct	s_data {
 	void	*img;
 	char	*addr;
@@ -9,6 +12,12 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+typedef struct	s_vars {
+	void		*mlx;
+	void		*win;
+}				t_vars;
+
+//원하는 좌표에 해당하는 주소에 color값을 넣는 함수
 void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -17,26 +26,36 @@ void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+int	key_hook(int keycode, t_vars *vars)
+{
+	printf("%d\n", keycode);
+	if(keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
+
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_data	image;
+	t_vars vars;
+	t_data image;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Hello World");
-	image.img = mlx_new_image(mlx_ptr, 500, 500);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello World");
+	image.img = mlx_new_image(vars.mlx, 500, 500);
 	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
 	for (int i = 0 ; i < 500 ; i++)
 	{
 		for (int j = 0 ; j < 500 ; j++)
 		{
-			//mlx_pixel_put (mlx_ptr, win_ptr, i, j, 0x00FFFFFF);
 			my_mlx_pixel_put(&image, i, j, 0x00FFFFFF);
 		}	
 	}
-	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
-	mlx_loop(mlx_ptr);
+	mlx_put_image_to_window(vars.mlx, vars.win, image.img, 0, 0);
+	mlx_key_hook(vars.win, key_hook, &vars); // esc key press event
+	mlx_loop(vars.mlx);
 
 	return (0);
 }
